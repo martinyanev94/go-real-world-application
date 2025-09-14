30ac3adcf663bdc61d68b915c50ea27a5a9bad26
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:20:31 -0400

    Add Lesson_21_Go_Tooling_Overview/part_1_hello_world.go

---
+++ Lesson_21_Go_Tooling_Overview/part_1_hello_world.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+go
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+
+
+go build -o hello
+
