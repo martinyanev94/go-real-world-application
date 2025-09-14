46009a21ca17ac936b8f57b27314d8c0e227989d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:20:37 -0400

    Add Lesson_21_Go_Tooling_Overview/part_7_http_profile_logger.go

---
+++ Lesson_21_Go_Tooling_Overview/part_7_http_profile_logger.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "net/http"
+    _ "net/http/pprof"
+)
+
+func main() {
+    go func() {
+        log.Println(http.ListenAndServe("localhost:6060", nil))
+    }()
+    
+    // Simulating heavy work
+    for {
+      // implementation
+    }
+}
