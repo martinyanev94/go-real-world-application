6b12d5824bd2542418e41f6cd99dacd537c8fd8d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:20:34 -0400

    Add Lesson_21_Go_Tooling_Overview/part_4_add_integers.go

---
+++ Lesson_21_Go_Tooling_Overview/part_4_add_integers.go
@@ -0,0 +1,14 @@
+// Add sums two integers and returns the result.
+func Add(x, y int) int {
+    return x + y
+}
+
+go
+// Add sums two integers and returns the result.
+func Add(x, y int) int {
+    return x + y
+}
+
+
+godoc -http :8080
+
