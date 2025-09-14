b158561ea2e53cd5a46d3b0e18367c019fdd66f6
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:21:55 -0400

    Add Lesson_3_Predeclared_Types_In_Go/part_2_data_types_example.go

---
+++ Lesson_3_Predeclared_Types_In_Go/part_2_data_types_example.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int = 10          // signed integer
+    var b uint = 20        // unsigned integer
+    var c float32 = 3.14   // floating-point number
+    var d float64 = 2.718  // double precision floating-point number
+
+    fmt.Println("Signed Integer:", a)
+    fmt.Println("Unsigned Integer:", b)
+    fmt.Println("Float32 Value:", c)
+    fmt.Println("Float64 Value:", d)
+}
