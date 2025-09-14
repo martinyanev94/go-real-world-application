f22e2c0cbacf870c1afe53dd0dfc0624273d6803
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:21:56 -0400

    Add Lesson_3_Predeclared_Types_In_Go/part_3_sunny_day_checker.go

---
+++ Lesson_3_Predeclared_Types_In_Go/part_3_sunny_day_checker.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var isSunny bool = true
+
+    if isSunny {
+        fmt.Println("It's a sunny day!")
+    } else {
+        fmt.Println("Looks like we need an umbrella.")
+    }
+}
