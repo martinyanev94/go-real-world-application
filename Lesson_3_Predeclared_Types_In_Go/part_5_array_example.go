d003bea4b904025f94281d737e1d8f61f79a0b6c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:21:59 -0400

    Add Lesson_3_Predeclared_Types_In_Go/part_5_array_example.go

---
+++ Lesson_3_Predeclared_Types_In_Go/part_5_array_example.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    var numbers [5]int // An array of 5 integers
+    numbers[0] = 1
+    numbers[1] = 2
+    numbers[2] = 3
+    numbers[3] = 4
+    numbers[4] = 5
+
+    fmt.Println("Array of numbers:", numbers)
+}
