a6c404d8ace08fcfb3cfb6ea6ec8b2716199e136
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:22:08 -0400

    Add Lesson_4_Type_Conversions_And_Literals_In_Go/part_3_literals_example.go

---
+++ Lesson_4_Type_Conversions_And_Literals_In_Go/part_3_literals_example.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    var integerLiteral int = 100                       // Integer literal
+    var floatLiteral float64 = 25.50                   // Floating-point literal
+    var booleanLiteral bool = true                      // Boolean literal
+    var stringLiteral string = "Hello, Go!"            // String literal
+    var runeLiteral rune = 'G'                          // Rune literal
+
+    fmt.Println("Integer Literal:", integerLiteral)
+    fmt.Println("Float Literal:", floatLiteral)
+    fmt.Println("Boolean Literal:", booleanLiteral)
+    fmt.Println("String Literal:", stringLiteral)
+    fmt.Println("Rune Literal:", runeLiteral)
+}
