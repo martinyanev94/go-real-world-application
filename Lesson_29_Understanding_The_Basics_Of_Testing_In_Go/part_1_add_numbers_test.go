2bfa2f7bbe4e4c67e5569f7a35800808837415f4
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:21:30 -0400

    Add Lesson_29_Understanding_The_Basics_Of_Testing_In_Go/part_1_add_numbers_test.go

---
+++ Lesson_29_Understanding_The_Basics_Of_Testing_In_Go/part_1_add_numbers_test.go
@@ -0,0 +1,13 @@
+func addNumbers(x, y int) int {
+    return x + y
+}
+package main
+
+import "testing"
+
+func Test_addNumbers(t *testing.T) {
+    result := addNumbers(2, 3)
+    if result != 5 {
+        t.Error("incorrect result: expected 5, got", result)
+    }
+}
