eb6db1a9092f1667ac23853a29f39d212e379af9
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-14 06:22:09 -0400

    Add Lesson_4_Type_Conversions_And_Literals_In_Go/part_4_literals_demo.go

---
+++ Lesson_4_Type_Conversions_And_Literals_In_Go/part_4_literals_demo.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var hexLiteral int = 0x1A // Hexadecimal representation of 26
+    var binaryLiteral int = 0b11010 // Binary representation of 26
+
+    fmt.Printf("Hexadecimal Literal: %d\n", hexLiteral)
+    fmt.Printf("Binary Literal: %d\n", binaryLiteral)
+}
